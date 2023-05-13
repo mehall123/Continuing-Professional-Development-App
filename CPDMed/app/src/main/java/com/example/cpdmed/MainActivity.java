b46596655e770eaefd7038e17c18ef2b63775d11
mehall123 <114657169+mehall123@users.noreply.github.com>
2023-05-13 20:30:20 +0100

    created basic home screen

--- CPDMed/app/src/main/java/com/example/cpdmed/MainActivity.java
+++ CPDMed/app/src/main/java/com/example/cpdmed/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.cpdmed;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLogin =(Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomeActivity();
+            }
+        });
+    }
+    public void openHomeActivity(){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
